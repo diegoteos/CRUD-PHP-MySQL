202e794660c1c50f49ebfff38c4269b64ad812c2
diegoteos <diegoteos@hotmail.com>
2022-07-09 08:20:04 -0600

    listo

---
+++ nosotros.php
@@ -0,0 +1,18 @@
+<?php
+include 'template/cabecera.php';
+?>
+
+
+<div class="p-5 bg-light">
+    <div class="container">
+        <h1 class="display-3">Nosotros</h1>
+        <p class="lead">Hola somos un grupo de personas que se dedica al desarrollo web</p>
+        <hr class="my-2">
+    </div>
+</div>
+
+
+
+<?php
+include 'template/pie.php';
+?>
---
+++ productos.php
@@ -0,0 +1,50 @@
+<?php
+include 'template/cabecera.php';
+?>
+
+<div class="col-md-3">
+    <div class="card">
+        <img class="card-img-top" src="./img/978846633212.gif" alt="">
+        <div class="card-body">
+            <h4 class="card-title">Padre Rico Padre Pobre</h4>
+            <a name="" id="" class="btn btn-primary" href="#" role="button">Ver más...</a>
+        </div>
+    </div>
+</div>
+<div class="col-md-3">
+    <div class="card">
+        <img class="card-img-top" src="./img/978846633212.gif" alt="">
+        <div class="card-body">
+            <h4 class="card-title">Padre Rico Padre Pobre</h4>
+            <a name="" id="" class="btn btn-primary" href="#" role="button">Ver más...</a>
+        </div>
+    </div>
+</div>
+<div class="col-md-3">
+    <div class="card">
+        <img class="card-img-top" src="./img/978846633212.gif" alt="">
+        <div class="card-body">
+            <h4 class="card-title">Padre Rico Padre Pobre</h4>
+            <a name="" id="" class="btn btn-primary" href="#" role="button">Ver más...</a>
+        </div>
+    </div>
+</div>
+<div class="col-md-3">
+    <div class="card">
+        <img class="card-img-top" src="./img/978846633212.gif" alt="">
+        <div class="card-body">
+            <h4 class="card-title">Padre Rico Padre Pobre</h4>
+            <a name="" id="" class="btn btn-primary" href="#" role="button">Ver más...</a>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+
+<?php
+include 'template/pie.php';
+?>
